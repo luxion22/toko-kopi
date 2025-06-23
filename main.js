eb3ac0f7fca2f05c9cbf8da68e831e48e722022c
luxion22 <agusdianaputra26@gmail.com>
2025-06-23 08:41:28 +0800

    Fade in animation

--- main.js
+++ main.js
@@ -64,3 +64,9 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    // Fade in animation on scroll
+    const observerOptions = {
+        threshold: 0.1,
+        rootMargin: '0px 0px -50px 0px'
+    };
